9ccfef2d01c3526e875fce7303c3ab3b4b8a144d
Renard <riuxzz@gmail.com>
2016-04-13 20:11:31 -0300

    Test

--- app/src/main/java/org/bienvenidoainternet/baiparser/MainActivity.java
+++ app/src/main/java/org/bienvenidoainternet/baiparser/MainActivity.java
@@ -194,6 +194,8 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    //merge test hola, no se ocupar git ww
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);

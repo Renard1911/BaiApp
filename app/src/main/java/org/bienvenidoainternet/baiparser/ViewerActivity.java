b1f9647cf318ee9916aa8a31ec062475b35bcfdf
Renard <riuxzz@gmail.com>
2016-04-14 17:58:28 -0300

    LICENSE

--- app/src/main/java/org/bienvenidoainternet/baiparser/ViewerActivity.java
+++ app/src/main/java/org/bienvenidoainternet/baiparser/ViewerActivity.java
@@ -25,6 +25,24 @@ import java.util.ArrayList;
 
 import layout.FragmentImage;
 
+/**
+ *   BaiApp - Bienvenido a internet Android Application
+ *   Copyright (C) 2016 Renard1911(https://github.com/Renard1911)
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class ViewerActivity extends AppCompatActivity implements FragmentImage.OnFragmentInteractionListener {
     private static final String EXTRA_FILELIST = "fileList", EXTRA_RELATIVEPOSITION = "position";
     private ViewPager imagePager;
